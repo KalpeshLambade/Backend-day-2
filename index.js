24a2639137ff9aaaff2de0adc24c65a5df450017
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-07-03 13:11:23 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require("express");
+// const express = require("express");
+
+// const app = express();
+
+import express  from "express";
 
 const app = express();
+
 
 app.get("/", (req, res) => {
   res.send("Hello Backend");
